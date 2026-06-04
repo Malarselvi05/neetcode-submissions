a5c1c3f63513e0106e83a992f40e3151bee6d08e
Malar <malarrajamani24@gmail.com>
2026-06-04 18:14:06 +0530

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+         x=[]
+         for word in s:
+          if word.isspace():
+            continue
+          if word.isalnum():
+            x.append(word.lower())
+         t=x[::-1]
+         if t==x:
+            return True
+         else:
+            return False
